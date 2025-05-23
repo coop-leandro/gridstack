bc61a0b351eabd70f86909b01d86cd5a30f1a595
Leandro <aluno5-labunesc@cooperja.com.br>
2025-05-23 13:16:27 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,3 @@ Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'loginProcess'])->name('login.process');
 
 Route::get('/dashboard', [DashboardEditor::class, 'mount'])->name('dashboard');
-Route::get('/setores/create', [SetorController::class, 'create'])->name('setores.create');
-Route::post('/setores', [SetorController::class, 'store'])->name('setores.store');
-Route::get('/setores/{setor}/usuarios', [SetorController::class, 'usuarios'])->name('setores.users');
-Route::post('/setores/{setor}/usuarios', [SetorController::class, 'adicionarUsuarios'])->name('setores.adicionarUsuarios');
